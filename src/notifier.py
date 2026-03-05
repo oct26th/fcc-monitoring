ea4dc4cbd8d322deef215cd3541f8de284c8aad9
oct26th <oct26th@users.noreply.github.com>
2026-03-05 08:53:52 +0800

    style: perfect icon match for notifications (office building icon)

--- src/notifier.py
+++ src/notifier.py
@@ -87,7 +87,7 @@ class Notifier:
         for code, records in grouped.items():
             # Get clean brand name from first record
             brand_name = records[0].applicant_name.split('(')[0].strip()
-            msg += f"📱 <b>{brand_name} ({code})</b>\n"
+            msg += f"🏢 <b>{brand_name} ({code})</b>\n"
             
             for r in records[:20]: # Show up to 20 per brand
                 # Use application type or description for the parentheses part
